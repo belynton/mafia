17ce70b6c0473e79da1aca899784ae3c0eae8b1c
Anton Belyakov <ABelyakov@egartech.ru>
2025-03-17 13:15:53 +0300

    join room now working and processed in one transaction

--- src/room/room.module.ts
+++ src/room/room.module.ts
@@ -4,10 +4,12 @@ import { RoomService } from "./room.service";
 import { Module } from '@nestjs/common';
 import { RoomController } from "./room.controller";
 import { UserService } from "src/user/user.service";
+import { User } from "src/user/users.entity";
+import { UserRoom } from "src/user-room/user-room.entity";
 
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Room])],
+  imports: [TypeOrmModule.forFeature([Room, User, UserRoom])],
   providers: [RoomService, UserService],
   controllers: [RoomController],
 })
--- src/room/room.service.ts
+++ src/room/room.service.ts
@@ -1,7 +1,7 @@
 import { InjectRepository } from "@nestjs/typeorm";
 import { Room } from "./room.entity";
 import { Inject, Injectable } from "@nestjs/common";
-import { Repository } from "typeorm";
+import { DataSource, Repository } from "typeorm";
 import { CreateRoomDto } from "src/dto/create.room.dto";
 import { UserService } from "src/user/user.service";
 import { UserRoom } from "src/user-room/user-room.entity";
@@ -17,6 +17,7 @@ export class RoomService {
         private userRepository: Repository<User>,
         @InjectRepository(UserRoom)
         private userRoomRepository: Repository<UserRoom>,
+        private dataSource: DataSource,
       ) {}
 
       async create(createRoomDto: CreateRoomDto): Promise<Room> {
@@ -26,16 +27,20 @@ export class RoomService {
 
       
       async join(roomId, userId: number): Promise<void> {
-        const room = await this.repository.findOne({
-            where: { id: roomId, },
-            relations: ['userRooms'],
-        });
-        if (!room) {
-            throw new Error('Room not found');
-        }
+        await this.dataSource.transaction(async (manager) => {
+            const roomRepository = manager.getRepository(Room);
+            const userRepository = manager.getRepository(User);
+            const userRoomRepository = manager.getRepository(UserRoom);
+
+            const room = await roomRepository.findOne({
+                where: { id: roomId, },
+                relations: ['userRooms'],
+            });
+            if (!room) {
+                throw new Error('Room not found');
+            }
     
-        // Проверяем, есть ли уже запись в UserRoom для данного пользователя и комнаты
-            const existingUserRoom = await this.userRoomRepository.findOne({
+            const existingUserRoom = await userRoomRepository.findOne({
                 where: { roomId, userId },
             });
 
@@ -43,17 +48,21 @@ export class RoomService {
                 throw new Error('User is already in the room');
             }
 
-            const user = await this.userRepository.findOne({ where: { id: userId } });
+            const user = await userRepository.findOne({ where: { id: userId } });
             if (!user) {
                 throw new Error('User not found');
             }
-
-            const newUserRoom = this.userRoomRepository.create({ roomId, userId });
-            await this.userRoomRepository.save(newUserRoom);
-
-            // Обновляем количество игроков в комнате
+            const newUserRoom = userRoomRepository.create({
+                roomId: room.id,
+                userId: user.id,
+            });
+            await userRoomRepository.save(newUserRoom);
+        
+            room.userRooms.push(newUserRoom)
             room.playerCount = (room.playerCount || 0) + 1;
-            await this.repository.save(room);
+            await roomRepository.save(room);
+        });
+
     }
 
 }
